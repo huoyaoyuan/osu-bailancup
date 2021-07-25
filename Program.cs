d66f910e54f947098159484f1361175ef4e984f8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-07-25 23:04:43 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace osu_bailancup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
