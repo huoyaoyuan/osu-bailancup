d09750a3714a6fe9dafabb58082e89de2735c67c
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-07-25 23:22:43 +0800

    osuapi

---
+++ CupManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu_bailancup
+{
+    class CupManager
+    {
+        private readonly string apiKey;
+        private readonly int mode;
+
+        public CupManager(string apiKey, int mode)
+        {
+            this.apiKey = apiKey;
+            this.mode = mode;
+        }
+    }
+}
---
+++ OsuApi.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    static class IsExternalInit { }
+}
+
+namespace osu_bailancup
+{
+    static class OsuApi
+    {
+        private static readonly HttpClient httpClient = new()
+        {
+            BaseAddress = new("https://osu.ppy.sh")
+        };
+
+        private static readonly JsonSerializerOptions options = new(JsonSerializerDefaults.Web);
+
+        public static async Task<ApiUser> GetUserAsync(string username, int mode, string apiKey)
+        {
+            using var response = await httpClient.PostAsJsonAsync("/get_user", new
+            {
+                u = username,
+                type = "string",
+                m = mode,
+                k = apiKey
+            }, options);
+            return await response.Content.ReadFromJsonAsync<ApiUser>(options);
+        }
+
+        public static async Task<Score[]> GetUserRecentAsync(int userId, int mode, string apiKey)
+        {
+            using var response = await httpClient.PostAsJsonAsync("/get_user_recent", new
+            {
+                u = userId,
+                type = "id",
+                m = mode,
+                limit = 1,
+                k = apiKey
+            }, options);
+            return await response.Content.ReadFromJsonAsync<Score[]>(options);
+        }
+    }
+
+    public record ApiUser(string username, int user_id);
+    public record Score(
+        int beatmap_id,
+        int score,
+        int maxcombo,
+        int count50,
+        int count100,
+        int count300,
+        int countmiss,
+        int countkatu,
+        int countgeki,
+        string rank)
+    {
+
+    }
+}
